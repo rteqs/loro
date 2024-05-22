ca8ba8bce8aa0eb5d7d85bcd8907f193a7914341
Ryan Teoh <ryan@latch.bio>
2024-05-22 13:37:18 -0700

    feat: implementing import & export Python bindings

--- crates/loro-python/src/lib.rs
+++ crates/loro-python/src/lib.rs
@@ -20,6 +20,32 @@ impl Loro {
         let text = self.0.get_text(id);
         LoroText(text)
     }
+
+    pub fn import(&mut self, snapshot: &str) -> PyResult<()> {
+        let snapshot_decoded = base64::decode(snapshot).unwrap();
+        self.0.import(&snapshot_decoded).unwrap();
+        Ok(())
+    }
+
+    pub fn import_update_batch(&mut self, data: &PyList) -> PyResult<()> {
+        let decoded_updates = data
+            .iter()
+            .map(|x| {
+                let update: &str = x.extract::<str>()?;
+                base64::decode(update)
+            })
+            .collect::<Vec<_>>();
+
+        if decoded_updates.is_empty() {
+            return Ok(());
+        }
+        Ok(self.0.import_batch(bytes)?)
+    }
+
+    pub fn export_snapshot(&self) -> PyResult<String> {
+        let snapshot = self.0.export_snapshot().unwrap();
+        Ok(base64::encode(snapshot))
+    }
 }
 
 #[pymethods]
