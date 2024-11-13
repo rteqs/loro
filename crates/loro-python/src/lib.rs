185b6aa548ed1fd8da8fb2fee5d02f4f8c601eb0
Ryan Teoh <ryan@latch.bio>
2024-11-13 11:44:29 -0800

    v1 encoding

--- crates/loro-python/src/lib.rs
+++ crates/loro-python/src/lib.rs
@@ -1,7 +1,8 @@
-use std::sync::Arc;
-
+use base64::prelude::*;
+use loro_internal::encoding::ExportMode;
 use loro_internal::{LoroDoc, TextHandler};
-use pyo3::prelude::*;
+use pyo3::{prelude::*, types::PyList};
+use std::borrow::Cow;
 
 #[pyclass]
 struct Loro(LoroDoc);
@@ -20,6 +21,37 @@ impl Loro {
         let text = self.0.get_text(id);
         LoroText(text)
     }
+
+    pub fn import_snapshot(&mut self, snapshot: &[u8]) -> PyResult<()> {
+        self.0.import(snapshot).unwrap();
+        Ok(())
+    }
+
+    pub fn import_update_batch(&mut self, data: &PyList) -> PyResult<()> {
+        let decoded_updates = data
+            .iter()
+            .filter_map(|x| {
+                let update: String = x.extract::<String>().ok()?.to_string();
+                BASE64_STANDARD.decode(update).ok()
+            })
+            .collect::<Vec<_>>();
+
+        if decoded_updates.is_empty() {
+            return Ok(());
+        }
+        self.0.import_batch(&decoded_updates[..]).unwrap();
+        Ok(())
+    }
+
+    pub fn export_snapshot(&self) -> PyResult<String> {
+        let snapshot = self
+            .0
+            .export(ExportMode::ShallowSnapshot(Cow::Owned(
+                self.0.oplog_frontiers().into(),
+            )))
+            .unwrap();
+        Ok(BASE64_STANDARD.encode(snapshot))
+    }
 }
 
 #[pymethods]
@@ -31,9 +63,9 @@ impl LoroText {
         Ok(())
     }
 
-    pub fn value(&self) -> String {
-        Arc::try_unwrap(self.0.get_value().into_string().unwrap()).unwrap_or_else(|x| (*x).clone())
-    }
+    // pub fn value(&self) -> String {
+    //     Arc::try_unwrap(self.0.get_value().into_string().unwrap()).unwrap_or_else(|x| (*x).clone())
+    // }
 }
 
 #[pymodule]
